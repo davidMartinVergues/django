c43ef27791460f1ffb93f5cefc3633e13e2152a5
David Martin Vergues <david@easomagno.com>
2023-10-18 21:16:53 +0200

    update

--- proyectos/prueba/apps/libro/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Autor(models.Model):
-    id = models.AutoField(primary_key = True)
-    nombre = models.CharField(max_length=200, blank=False, null=False)
-    apeliidos = models.CharField(max_length=220, blank=False, null=False)
-    nacionaldad = models.CharField(max_length=200, blank=False, null=False)
-    descripcion = models.TextField(blank=False, null=False)
