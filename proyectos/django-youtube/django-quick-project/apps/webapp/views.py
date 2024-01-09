31c5c40f656433663d36def8e08b1ac8cfc1a313
David Martin Vergues <david@easomagno.com>
2024-01-09 11:19:29 +0100

    update

--- proyectos/django-youtube/django-quick-project/apps/webapp/views.py
+++ proyectos/django-youtube/django-quick-project/apps/webapp/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-# Create your views here.
+
+from .forms import CreateUserForm, LoginForm
+
 
 def home(request):
     # return HttpResponse('Hello world')
     return render(request,'webapp/index.html')
 
+# - Register a use
+
+def register(request):
+    
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+            form.save() # sending data from form 
+
+            #return redirect('')
+
+    
+    context = {"form":form}
+
+    return render(request,'webapp/register.html',context=context)
+    
 
